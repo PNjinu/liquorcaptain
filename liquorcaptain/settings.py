567dcbd8457dd3ddff17da9872ab9de297a0b8d9
Patrick Njinu Kuria <pkuria@fcb.co.ke>
2022-12-08 15:32:58 +0300

    Remove a hardcoded Django secret key

--- liquorcaptain/settings.py
+++ liquorcaptain/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dz=p3igtktjz36d-!g9#tx)l@qsqz01f@gh26ju0foo(wyg664'
+# SECRET_KEY = 'django-insecure-dz=p3igtktjz36d-!g9#tx)l@qsqz01f@gh26ju0foo(wyg664'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
