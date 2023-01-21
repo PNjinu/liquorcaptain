ea31a322baf523606747421d5165ba50a8e82d32
PNjinu <kuria144@gmail.com>
2023-01-21 12:02:49 +0300

    Changed the database and separated settings files

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
 # Create your models here.
+class Supplier(models.Model):
+    name = models.CharField(max_length=255)
+    contact_info = models.CharField(max_length=255)
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    buying_price = models.DecimalField(max_digits=10, decimal_places=2)
+    selling_price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField()
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE) #kama ni beer ama whiskey
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=255)
+    contact_info = models.CharField(max_length=255)
+    loyalty_points = models.PositiveIntegerField()
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+    total_cost = models.DecimalField(max_digits=10, decimal_places=2)
+    order_date = models.DateTimeField()
+
+class Inventory(models.Model):
+    product = models.OneToOneField(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+class Transaction(models.Model):
+    order = models.OneToOneField(Order, on_delete=models.CASCADE)
+    amount_paid = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_method = models.CharField(max_length=255)
+
+class Tax(models.Model):
+    rate = models.DecimalField(max_digits=4, decimal_places=2)
+
