e0c3075f2369c6365e10779627753b7e9fadc871
Patrick Kuria <kuria144@gmail.com>
2023-02-07 15:12:46 +0300

    HTML Template Configured to work with django

--- shop/urls.py
+++ shop/urls.py
@@ -5,5 +5,10 @@ urlpatterns = [
     path('', views.landing, name="landing"),
     path('about', views.about, name='about'),
     path('blog', views.blog, name='blog'),
-    path('contact', views.contact, name='contact')
+    path('blog_single', views.blog_single, name='blog_single'),
+    path('product_single', views.product_single, name='product_single'),
+    path('contact', views.contact, name='contact'),
+    path('cart', views.cart, name="cart"),
+    path('checkout', views.checkout, name="checkout"),
+    path('store', views.store, name="store"),
 ]
--- shop/views.py
+++ shop/views.py
@@ -11,5 +11,21 @@ def about(request):
 def blog(request):
     return render(request, "shop/blog.html")
 
+def blog_single(request):
+    return render(request, "shop/blog-single.html")
+
 def contact(request):
     return render(request, "shop/contact.html")
+
+def store(request):
+    return render(request, "shop/store.html")
+
+def product_single(request):
+    return render(request, "shop/product-single.html")
+
+def cart(request):
+    return render(request, "shop/cart.html")
+
+def checkout(request):
+    return render(request, "shop/checkout.html")
+
